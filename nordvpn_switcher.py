31ac05ca2647853e013291cef59e1cbca29501ef
Neiki <67499407+NeikiDev@users.noreply.github.com>
2022-06-03 22:26:09 +0200

    Update nordvpn_switcher.py

--- nordvpn_switcher.py
+++ nordvpn_switcher.py
@@ -1,19 +1,14 @@
 import os
-import time
 import subprocess
-
 def banner():
  os.system("clear")
  os.system("figlet Nordvpn Switcher")
  print("Code made by: CryptoCat44 and NeikiDev")
- print("Version: 1.0.3")
+ print("Version: 1.0.3 | https://github.com/neikidev")
  print("")
-
-
 login_status = "Default"
-
+banner()
 print("Searching for NordVPN connection, please wait...!\n\n")
-
 try:
     login_status = subprocess.check_output(["nordvpn", "account"])
 except Exception as e:
@@ -22,23 +17,18 @@ except Exception as e:
     os.system("nordvpn account")
     input("Press any Key to Exit!...")
     exit()
-
 login_status = str(login_status)
-
 account_email = login_status.split("Email Address: ")[1].split("\\n")[0]
 account_info = login_status.split("VPN Service: ")[1].split('\\n')[0]
-
 print('We found this Account Informations: ')
 print('Email Address: '+account_email)
 print('VPN Service: '+account_info)
-
 right=str(input("Is this the right Informations? (Y/n) :"))
 if((right=="N") or (right=="n")):
     print("The Informations are Wrong?")
     print('Please relogin with "nordvpn login" or check your account with "nordvpn account!"\n')
     input("Press any Key to Exit!...")
     exit()
-
 banner()
 os.system("bash vpn.sh")
 
